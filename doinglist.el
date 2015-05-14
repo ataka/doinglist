65465573eda7cf5fadb142ca51ac8329f233be32
ataka <masayuki.ataka@gmail.com>
2015-05-14 23:20:49 +0900

    New function doinglist-insert-new-item

--- doinglist.el
+++ doinglist.el
@@ -25,3 +25,25 @@
   (find-file (expand-file-name (format-time-string "doing-%Y%m%d.txt")
                                doinglist-data-directory))
   (doinglist-mode))
+
+;;
+;; Functions for doinglist-mode
+;;
+
+(defun doinglist-insert-new-item ()
+  (interactive)
+  (let ((level (doinglist-get-level)))
+    (insert (doinglist-new-item level))))
+
+(defun doinglist-get-level ()
+  (save-excursion
+    (beginning-of-line)
+    (if (and (not (bobp))
+             (progn (forward-line -1)
+                    (looking-at "^\\[[ x]\\]\\([[:space:]]+\\)")))
+        (/ (1- (length (match-string 1))) 2)
+      0)))
+
+(defun doinglist-new-item (level)
+  (let* ((indent (* level 2)))
+    (format (concat "[ ] %" (number-to-string indent) "s") "")))
